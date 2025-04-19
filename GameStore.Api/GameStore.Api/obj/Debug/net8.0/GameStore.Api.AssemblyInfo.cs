62b0880d263ab190da279f84fbb4f7673b9f5838
Yazeed Williams <yazeedwilliams13@gmail.com>
2025-04-19 21:15:37 +0200

    Test build 3

--- GameStore.Api/GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ee9f50b4d0cfa1e403b0bbc1a95fbf127d0719e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b045c91c6aa94e4ff545e4148f0b3a096144d23")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
