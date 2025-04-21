c5d30fcfaa751c49d2fc8ef8754dd0224f3871ef
Yazeed Williams <yazeedwilliams13@gmail.com>
2025-04-21 21:36:46 +0200

    GameDTO.cs

--- GameStore.Api/GameStore.Api/Program.cs
+++ GameStore.Api/GameStore.Api/Program.cs
@@ -1,6 +1,18 @@
+using GameStore.Api.DTOs;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+List<GameDTO> games = [
+    new (1, "Street Fighter II", "Fighting", 19.99M, new DateOnly(1992, 7, 15)),
+    new (2, "Final Fantasy XIV", "Roleplaying", 59.99M, new DateOnly(2010, 9, 30)),
+    new (1, "Fifa 23", "Sports", 69.99M, new DateOnly(2022, 9, 27))
+    ];
+
+// Get /games
+app.MapGet("games", () => games);
+
+// Get /games/1
+app.MapGet("games/{ID}", (int id) => games.Find(game => game.ID == id));
 
 app.Run();
