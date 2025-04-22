cfb926b4269600c0535f811d97d3c79364ccebc5
Yazeed Williams <yazeedwilliams13@gmail.com>
2025-04-22 20:55:15 +0200

    New class to clean program.cs

--- GameStore.Api/GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3866a601215c40bd4f5119aa693dfaf9e48715fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+137c3fd5f594d47023226f917ead6f839b54615c")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
